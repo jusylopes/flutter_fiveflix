2ca6be75c564e09f5abd4b3a08610e01da038823
Andréa <rangelfoco2@gmail.com>
2024-05-04 21:22:57 -0300

    feat: create mockup movies detail

--- lib/models/mockup/movie/mockup_movie_detail.dart
+++ lib/models/mockup/movie/mockup_movie_detail.dart
@@ -4,21 +4,164 @@ abstract class MockupMovieDetail {
   static List<MovieDetailModel> movieDetail() {
     return [
       MovieDetailModel(
-          backdropPath: '/di47xqYMCYpjqwnqNlO17X5qXMX.jpg',
+          backdropPath: '/qrGtVFxaD8c7et0jUtaYhyTzzPg.jpg',
           genres: [
-            Genre(id: 35, name: "Comedy"),
-            Genre(id: 80, name: "Crime"),
+            Genre(id: 28, name: "Action"),
+            Genre(id: 878, name: "Science Fiction"),
+            Genre(id: 12, name: "Adventure"),
           ],
-          id: 12153,
-          imdbId: "tt0381707",
+          id: 823464,
+          imdbId: "tt14539740",
           overview:
-              "Two FBI agent brothers, Marcus and Kevin Copeland, accidentally foil a drug bust. To avoid being fired they accept a mission escorting a pair of socialites to the Hamptons--but when the girls are disfigured in a car accident, they refuse to go. Left without options, Marcus and Kevin decide to pose as the sisters, transforming themselves from black men into rich white women.",
-          popularity: 143.356,
-          posterPath: "/aHTUpo45qy9QYIOnVITGGqLoVcA.jpg",
-          releaseDate: DateTime.parse("2004-06-23"),
-          title: "White Chicks",
-          voteAverage: 6.951),
-      
+              "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+          popularity: 1818.259,
+          posterPath: "/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
+          releaseDate: DateTime.parse("2024-03-27"),
+          title: "Godzilla x Kong: The New Empire",
+          voteAverage: 6.513),
+      MovieDetailModel(
+          backdropPath: '/jnE1GA7cGEfv5DJBoU2t4bZHaP4.jpg',
+          genres: [
+            Genre(id: 28, name: "Action"),
+            Genre(id: 878, name: "Science Fiction"),
+          ],
+          id: 1094844,
+          imdbId: "tt26785742",
+          overview:
+              "Recognizing the destructive power of its captive giant Ape, the military makes its own battle-ready A.I., Mecha Ape. But its first practical test goes horribly wrong, leaving the military no choice but to release the imprisoned giant ape to stop the colossal robot before it destroys downtown Chicago.",
+          popularity: 1639.584,
+          posterPath: "/dJaIw8OgACelojyV6YuVsOhtTLO.jpg",
+          releaseDate: DateTime.parse("2023-03-24"),
+          title: "Ape vs. Mecha Ape",
+          voteAverage: 5.872),
+      MovieDetailModel(
+          backdropPath: '/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg',
+          genres: [
+            Genre(id: 878, name: "Science Fiction"),
+            Genre(id: 12, name: "Adventure"),
+          ],
+          id: 693134,
+          imdbId: "tt15239678",
+          overview:
+              "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+          popularity: 1390.639,
+          posterPath: "/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+          releaseDate: DateTime.parse("2024-02-27"),
+          title: "Dune: Part Two",
+          voteAverage: 8.235),
+      MovieDetailModel(
+          backdropPath: '/4woSOUD0equAYzvwhWBHIJDCM88.jpg',
+          genres: [
+            Genre(id: 28, name: "Action"),
+            Genre(id: 27, name: "Horror"),
+            Genre(id: 53, name: "Thriller"),
+          ],
+          id: 1096197,
+          imdbId: "tt16253418",
+          overview:
+              "Characters from different backgrounds are thrown together when the plane they're travelling on crashes into the Pacific Ocean. A nightmare fight for survival ensues with the air supply running out and dangers creeping in from all sides.",
+          popularity: 1245.817,
+          posterPath: "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
+          releaseDate: DateTime.parse("2024-01-18"),
+          title: "No Way Up",
+          voteAverage: 6.329),
+      MovieDetailModel(
+          backdropPath: '/kYgQzzjNis5jJalYtIHgrom0gOx.jpg',
+          genres: [
+            Genre(id: 16, name: "Animation"),
+            Genre(id: 28, name: "Action"),
+            Genre(id: 10751, name: "Family"),
+            Genre(id: 35, name: "Comedy"),
+            Genre(id: 14, name: "Fantasy"),
+          ],
+          id: 1011985,
+          imdbId: "tt21692408",
+          overview:
+              "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+          popularity: 1230.013,
+          posterPath: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+          releaseDate: DateTime.parse("2024-03-02"),
+          title: "Kung Fu Panda 4",
+          voteAverage: 7.132),
+      MovieDetailModel(
+          backdropPath: '/tpiqEVTLRz2Mq7eLq5DT8jSrp71.jpg',
+          genres: [
+            Genre(id: 878, name: "Science Fiction"),
+            Genre(id: 28, name: "Action"),
+            Genre(id: 18, name: "Drama"),
+          ],
+          id: 934632,
+          imdbId: "tt23137904",
+          overview:
+              "The rebels gear up for battle against the ruthless forces of the Motherworld as unbreakable bonds are forged, heroes emerge — and legends are made.",
+          popularity: 1187.439,
+          posterPath: "/cxevDYdeFkiixRShbObdwAHBZry.jpg",
+          releaseDate: DateTime.parse("2024-04-19"),
+          title: "Rebel Moon - Part Two: The Scargiver",
+          voteAverage: 6.117),
+      MovieDetailModel(
+          backdropPath: '/5Eip60UDiPLASyKjmHPMruggTc4.jpg',
+          genres: [
+            Genre(id: 27, name: "Horror"),
+            Genre(id: 9648, name: "Mystery"),
+            Genre(id: 53, name: "Thriller"),
+          ],
+          id: 1041613,
+          imdbId: "tt23137390",
+          overview:
+              "An American nun embarks on a new journey when she joins a remote convent in the Italian countryside. However, her warm welcome quickly turns into a living nightmare when she discovers her new home harbours a sinister secret and unspeakable horrors.",
+          popularity: 892.57,
+          posterPath: "/fdZpvODTX5wwkD0ikZNaClE4AoW.jpg",
+          releaseDate: DateTime.parse("2024-03-20"),
+          title: "Immaculate",
+          voteAverage: 6.3),
+      MovieDetailModel(
+          backdropPath: '/6a3aiSYNqABoV1Fq8n10LMOBxhH.jpg',
+          genres: [
+            Genre(id: 28, name: "Action"),
+            Genre(id: 53, name: "Thriller"),
+          ],
+          id: 821937,
+          imdbId: "tt13899926",
+          overview:
+              "A container truck driver, Hùng, lives an idyllic life with his little daughter, An. The father and daughter become the closest companions in every journey with their orange container truck. Life goes on like that until An has to leave her father to go to school. One day, Hùng is informed that An is suffering from severe depression. Relying on his old skills in the past and finding out the truth, Hùng becomes enraged and painful to know that his little girl was kidnapped by a male stranger. Starting his lone and intense journey looking for and chasing after the unknown abuser, Hùng realizes that to hunt down that psychopath he has to counter the massive underground forces behind him.",
+          popularity: 843.881,
+          posterPath: "/8Nw5v7wPfa2FwbKyx61K2nIBcKa.jpg",
+          releaseDate: DateTime.parse("2022-05-20"),
+          title: "578: Magnum",
+          voteAverage: 5.05),
+      MovieDetailModel(
+          backdropPath: '/qGJASuD3fs9ZBxuEZoxCLVidVq8.jpg',
+          genres: [
+            Genre(id: 12, name: "Adventure"),
+            Genre(id: 28, name: "Action"),
+            Genre(id: 18, name: "Drama"),
+          ],
+          id: 845111,
+          imdbId: "tt12672620",
+          overview:
+              "D'Artagnan, on a quest to rescue the abducted Constance, runs into the mysterious Milady de Winter again. The tension between the Catholics and the Protestants finally escalates, as the king declares war — forcing the now four musketeers into battle. But as the war goes on, they are tested physically, mentally and emotionally.",
+          popularity: 796.029,
+          posterPath: "/rtosxP5sXuoRFPH4sVbMccLIPiV.jpg",
+          releaseDate: DateTime.parse("2023-12-13"),
+          title: "The Three Musketeers: Milady",
+          voteAverage: 6.5),
+      MovieDetailModel(
+          backdropPath: '/v5XyXZe8FADw8iHupB4L7QOAwH9.jpg',
+          genres: [
+            Genre(id: 16, name: "Animation"),
+            Genre(id: 35, name: "Comedy"),
+            Genre(id: 10751, name: "Family"),
+          ],
+          id: 748783,
+          imdbId: "tt5779228",
+          overview:
+              "Garfield, the world-famous, Monday-hating, lasagna-loving indoor cat, is about to have a wild outdoor adventure! After an unexpected reunion with his long-lost father – scruffy street cat Vic – Garfield and his canine friend Odie are forced from their perfectly pampered life into joining Vic in a hilarious, high-stakes heist.",
+          popularity: 757.742,
+          posterPath: "/zK2sFxZcelHJRPVr242rxy5VK4T.jpg",
+          releaseDate: DateTime.parse("2024-04-30"),
+          title: "The Garfield Movie",
+          voteAverage: 7.265),
     ];
   }
 
