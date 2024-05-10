fc159ff9e50f7df09fb684c60e0cd70bed9da27e
AlexandreCalmonJr <alexandrecalmonjunior@gmail.com>
2024-05-09 21:29:19 -0300

    feat : media detail header image

--- lib/screens/media_detail.dart/media_detail_header_image.dart
+++ lib/screens/media_detail.dart/media_detail_header_image.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fiveflix/screens/widgets/transparent_gradient_container.dart';
+import 'package:flutter_fiveflix/utils/colors.dart';
+import 'package:flutter_fiveflix/utils/strings.dart';
 
 class MediaDetailHeaderImage extends StatelessWidget {
   const MediaDetailHeaderImage({
-    super.key,
+    super.key, required this.posterPathMedia,
   });
+ 
+  final String posterPathMedia;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+   double screenHeight = MediaQuery.of(context).size.height;
+   
+    return TransparentGradientContainer(
+            height: screenHeight / 2.0,
+            child: Container(
+              color: AppColors.backgroundColor,
+              child: Image.network(
+                AppStrings.urlImagePoster + posterPathMedia,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+                color: const Color.fromRGBO(255, 255, 255, 0.8),
+                colorBlendMode: BlendMode.modulate,
+              ),
+            ),
+          );
   }
 }
