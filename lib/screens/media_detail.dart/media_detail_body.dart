370bdeffee71e3ade2c04e73761d0accc46199a0
Evelyn <evelyn.vanielly@gmail.com>
2024-05-11 21:02:30 -0300

    feat: star rating

--- lib/screens/media_detail.dart/media_detail_body.dart
+++ lib/screens/media_detail.dart/media_detail_body.dart
@@ -26,6 +26,30 @@ class MediaDetailBody extends StatelessWidget {
             genresMovie: movie.genres,
             wrapAlignment: WrapAlignment.start,
           ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              const Icon(
+                Icons.star,
+                color: Color.fromARGB(255, 248, 212, 95),
+                size: 20,
+              ),
+              const SizedBox(
+                width: 5.0,
+              ),
+              //      const  SizedBox(height: 50.0,),
+
+              Text(
+                movie.voteAverage.toStringAsFixed(1),
+                style: const TextStyle(
+                  fontFamily: 'Helvetica',
+                  fontSize: 15.5,
+                  color: Color.fromARGB(175, 143, 152, 173),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
           Text(
             'Story Line',
             style: Theme.of(context).textTheme.titleLarge,
