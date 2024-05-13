71a988bad9ba1bf16e114af9db323a3fa0c73447
lucasnoc <lucasviniciusrq@gmail.com>
2024-05-13 13:44:51 -0300

    TESTE-JUSY

--- lib/models/mockup/serie/mockup_serie_detail.dart
+++ lib/models/mockup/serie/mockup_serie_detail.dart
@@ -4,28 +4,162 @@ abstract class MockupSerieDetail {
   static List<SerieDetailModel> serieDetail() {
     return [
       SerieDetailModel(
-        backdropPath: "backdropPath",
+        backdropPath: "/2sjy8Tu8PJGFQI4Foz4682qk8Mw.jpg",
         genres: [
           Genre(id: 1, name: ""),
         ],
-        id: 1,
-        numberOfSeasons: 1,
-        overview: "overview",
-        popularity: 1,
-        posterPath: "posterPath",
-        releaseDate: DateTime.parse("2004-06-23"),
-        name: "name",
-        voteAverage: 1,
+        id: 4447,
+        numberOfSeasons: 12,
+        overview:
+            "Every member of CSI is a suspect when one of the team's enemies is murdered. When the investigation becomes deadly, Samantha Owens ends up having to be rescued.",
+        popularity: 6,
+        posterPath: "/9Bx84fpDsemS7qgS2QKDjQfKB0f.jpg",
+        releaseDate: DateTime.parse("2002-09-23"),
+        name: "CSI: Miami",
+        voteAverage: 6.8,
         seasons: [
           Season(
-              airDate: DateTime.parse("2004-06-23"),
+              airDate: DateTime.parse("2004-06-29"),
               episodeCount: 1,
-              id: 1,
-              name: "name",
+              id: 4447,
+              name: "Specials",
               overview: "overvie",
-              posterPath: "posterPath",
+              posterPath: "/AqA7WOGR8Ln68vN8FS3minsued8.jpg",
               seasonNumber: 1,
-              voteAverage: 1),
+              voteAverage: 6),
+        ],
+      ),
+      SerieDetailModel(
+        backdropPath: "/cvlLBcQWpO9X21jDHhgPJnE2aVq.jpg",
+        genres: [
+          Genre(id: 1, name: ""),
+        ],
+        id: 1037502,
+        numberOfSeasons: 17,
+        overview:
+            "The CSI unit faces the final showdown with the Gig Harbor killer whose motives are finally revealed. Meanwhile, Nick Stokes makes a decision that will affect the entire team moving forward.",
+        popularity: 8,
+        posterPath: "/9Bx84fpDsemS7qgS2QKDjQfKB0f.jpg",
+        releaseDate: DateTime.parse("2002-02-23"),
+        name: "CSI: Crime Scene Investigation",
+        voteAverage: 9.3,
+        seasons: [
+          Season(
+              airDate: DateTime.parse("2003-09-24"),
+              episodeCount: 1,
+              id: 3795,
+              name: "Season 4",
+              overview: "overvie",
+              posterPath: "/zyoFAPWXqSN4sSmmgbe20yWCJmF.jpg",
+              seasonNumber: 4,
+              voteAverage: 6),
+        ],
+      ),
+      SerieDetailModel(
+        backdropPath: "/cvlLBcQWpO9X21jDHhgPJnE2aVq.jpg",
+        genres: [
+          Genre(id: 1, name: ""),
+        ],
+        id: 5227751,
+        numberOfSeasons: 26,
+        overview:
+            "In the criminal justice system, sexually-based offenses are considered especially heinous. In New York City, the dedicated detectives who investigate these vicious felonies are members of an elite squad known as the Special Victims Unit. These are their stories.",
+        popularity: 6.4,
+        posterPath: "/onmSVwYsPMYtO8OjLdjS8FfRNKb.jpg",
+        releaseDate: DateTime.parse("2002-02-23"),
+        name: "Law & Order: Special Victims Unit",
+        voteAverage: 5,
+        seasons: [
+          Season(
+              airDate: DateTime.parse("2003-09-24"),
+              episodeCount: 2,
+              id: 8889,
+              name: "Season 4",
+              overview:
+                  "Season 13 dealt with the departure of Detective Elliot Stabler from the Special Victims Unit after a shooting in the squad room.",
+              posterPath: "/noc79PeGMcZw2qCiVbyQV8B6q42.jpg",
+              seasonNumber: 4,
+              voteAverage: 6),
+        ],
+      ),
+      SerieDetailModel(
+        backdropPath: "/nmg2lY4QuyXQrAnrC2lRblK5rT6.jpg",
+        genres: [
+          Genre(id: 1, name: ""),
+        ],
+        id: 5170854,
+        numberOfSeasons: 12,
+        overview:
+            "An unexpectedly complex case brings back painful memories for Simone. Jules and Blue make a high-stakes bet on who can finish their procedure log first. Richard suspects Winston is avoiding him.",
+        popularity: 9,
+        posterPath: "/onmSVwYsPMYtO8OjLdjS8FfRNKb.jpg",
+        releaseDate: DateTime.parse("2024-05-09"),
+        name: "She Used to Be Mine",
+        voteAverage: 8.7,
+        seasons: [
+          Season(
+              airDate: DateTime.parse("2003-09-24"),
+              episodeCount: 2,
+              id: 1557,
+              name: "ShondaLand",
+              overview:
+                  "Owen and Teddy have the day off but encounter an injured civilian. Monica enlists Amelia's help with an operation on a young patient. The interns complete their procedure logs, allowing them back into the OR.",
+              posterPath: "/jcEl8SISNfGdlQFwLzeEtsjDvpw.jpg",
+              seasonNumber: 7,
+              voteAverage: 8),
+        ],
+      ),
+      SerieDetailModel(
+        backdropPath: "/oS3nip9GGsx5A7vWp8A1cazqJlF.jpg",
+        genres: [
+          Genre(id: 1, name: ""),
+        ],
+        id: 5170854,
+        numberOfSeasons: 12,
+        overview:
+            "Nick faces off against his greatest foe yet as it seems no weapon can defeat him. At the Spice Shop, Monroe, Rosalee and Eve search for answers and stumble upon a rare potion that may help the fight. Elsewhere, Capt. Renard and Adalind try to keep Diana and baby Kelly safe as the threat has its eyes set on the children.",
+        popularity: 9,
+        posterPath: "/iOptnt1QHi6bIHmOq6adnZTV0bU.jpg",
+        releaseDate: DateTime.parse("2010-05-09"),
+        name: "Grimm",
+        voteAverage: 8.7,
+        seasons: [
+          Season(
+              airDate: DateTime.parse("2011-10-28"),
+              episodeCount: 2,
+              id: 50767,
+              name: "Season 1",
+              overview: "",
+              posterPath: "/83D3Obk5yPJrtsMgh72d4C0VgD6.jpg",
+              seasonNumber: 7,
+              voteAverage: 8),
+        ],
+      ),
+      SerieDetailModel(
+        backdropPath: "/nVRyd8hlg0ZLxBn9RaI7mUMQLnz.jpg",
+        genres: [
+          Genre(id: 1, name: ""),
+        ],
+        id: 6904,
+        numberOfSeasons: 7,
+        overview:
+            "The Creepy. The Demented. The Unexplained. The Unearthly. Sam Winchester grew up hunting such terrifying things. But that’s all past. Law school beckons him. So does safety and normalcy. That is, until Sam’s estranged brother Dean appears with troubling news: their father has disappeared, a man who’s hunted evil for 22 years. So to find their father, the brothers must hunt what their father hunts…and Sam must return to the life he’d rather leave behind.",
+        popularity: 9,
+        posterPath: "/6VmmiImmxcspyjcm4qKLGVYlPh6.jpg",
+        releaseDate: DateTime.parse("2010-05-09"),
+        name: "Grimm",
+        voteAverage: 8.3,
+        seasons: [
+          Season(
+              airDate: DateTime.parse("2011-10-28"),
+              episodeCount: 2,
+              id: 6905,
+              name: "Season 1",
+              overview:
+                  "They're back the ghouls and vampires, the ghosts and spirits and even more terrifying entities. And wherever these evil beings are, you'll find brothers Sam and Dean Winchester, hunters on the trail of the demon who murdered their mother…and of any other unholy agent of the Supernatural. Join the brothers in their mind-bending, bone-chilling Season 2 road trip across America a journey deeper into the mystery of their own destinies.",
+              posterPath: "/f4NzMUpOqwzut7jML4IUZcHqkLt.jpg",
+              seasonNumber: 4,
+              voteAverage: 6),
         ],
       ),
     ];
