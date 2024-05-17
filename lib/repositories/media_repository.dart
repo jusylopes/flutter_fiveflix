17184f35e8f29e0b29b54591dd8d165d93d083eb
jusylopes <jusylopes@outlook.com>
2024-05-17 10:08:58 -0300

    feat: add request api media

--- lib/repositories/media_repository.dart
+++ lib/repositories/media_repository.dart
@@ -1,54 +1,31 @@
-import 'package:flutter_fiveflix/models/mockup/movie/mockup_movie_detail.dart';
-import 'package:flutter_fiveflix/models/mockup/movie/mockup_popular.dart';
-import 'package:flutter_fiveflix/models/mockup/serie/mockup_popular_serie.dart';
-import 'package:flutter_fiveflix/models/mockup/serie/mockup_serie_detail.dart';
-import 'package:flutter_fiveflix/models/movie_detail_model.dart';
-import 'package:flutter_fiveflix/models/popular_movie_model.dart';
-import 'package:flutter_fiveflix/models/popular_serie_model.dart';
-import 'package:flutter_fiveflix/models/serie_detail_model.dart';
+import 'package:flutter_fiveflix/datasources/dio_http_datasource.dart';
 
 class MediaRepository {
-  Future<List<PopularMovieModel>> getPopularMovies() async {
+  MediaRepository(this._datasource);
+  final HttpDatasource _datasource;
+
+  Future<List<T>> getPopularListMedia<T>(
+      {required endpoint, required T Function(dynamic) fromJson}) async {
     try {
-      await Future.delayed(const Duration(seconds: 1));
-      return MockupPopularMovie.getPopularMoviesSortedByPopularity();
+
+      final responseApi = await _datasource.get(endpoint);
+      List<T> listMedias =
+          responseApi['results'].map<T>((media) => fromJson(media)).toList();
+
+      return listMedias;
     } catch (e) {
       rethrow;
     }
   }
 
-  Future<List<PopularSerieModel>> getPopularSeries() async {
-    try {
-      await Future.delayed(const Duration(seconds: 1));
-      return MockupPopularSerie.mockPopularSerie();
-    } catch (e) {
+  Future getMediaDetail({required int id}) async {
+    try {} catch (e) {
       rethrow;
     }
   }
 
-  Future<MovieDetailModel> getMovieDetail({required int id}) async {
-    try {
-      await Future.delayed(const Duration(seconds: 1));
-      return MockupMovieDetail.getMovieById(id);
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<SerieDetailModel> getSerieDetail({required int id}) async {
-    try {
-      await Future.delayed(const Duration(seconds: 1));
-      return MockupSerieDetail.getSerieById(id);
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Future<List<PopularMovieModel>> searchMedia({required String query}) async {
-    try {
-      await Future.delayed(const Duration(seconds: 2));
-      return MockupPopularMovie.searcMoviesByQuery(query: query);
-    } catch (e) {
+  Future searchMedia({required String query}) async {
+    try {} catch (e) {
       rethrow;
     }
   }
