ad20b15210c411007c2b60cd0663d5f3f8927fdc
jusylopes <jusylopes@outlook.com>
2024-05-23 16:47:18 -0300

    feat: dio exception

--- lib/datasources/http_datasource.dart
+++ lib/datasources/http_datasource.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fiveflix/datasources/dio_http_datasource.dart';
+import 'package:flutter_fiveflix/utils/strings.dart';
 
 class DioHttpDatasource implements HttpDatasource {
   final Dio _dio;
@@ -13,15 +14,33 @@ class DioHttpDatasource implements HttpDatasource {
       final result = await _dio.get(url);
       return result.data;
     } on DioException catch (e) {
-      if (e.response != null) {
-        debugPrint(e.response!.data.toString());
-        debugPrint(e.response!.headers.toString());
-        debugPrint(e.response!.requestOptions.toString());
-      } else {
-        debugPrint(e.requestOptions.toString());
-        debugPrint(e.message);
-      }
-      rethrow;
+      _logDioException(e);
+      throw handleDioException(e);
     }
+  }
+
+  void _logDioException(DioException e) {
+    if (e.response != null) {
+      debugPrint('Error Response Data: ${e.response!.data}');
+      debugPrint('Error Response Headers: ${e.response!.headers}');
+      debugPrint('Error Request Options: ${e.response!.requestOptions}');
+    } else {
+      debugPrint('Error Request Options: ${e.requestOptions}');
+      debugPrint('Error Message: ${e.message}');
+    }
+  }
+
+  String handleDioException(DioException e) {
+    if (e.type == DioExceptionType.badResponse && e.response != null) {
+      switch (e.response!.statusCode) {
+        case 401:
+          return AppStrings.error401;
+        case 404:
+          return AppStrings.error404;
+        default:
+          return AppStrings.errorDefault;
+      }
+    }
+    return AppStrings.errorDefault;
   }
 }
