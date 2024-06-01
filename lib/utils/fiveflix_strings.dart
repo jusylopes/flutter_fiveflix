130db7f120a7bf53ec99e18f7bfc1579e8d6ed6c
jusylopes <jusylopes@outlook.com>
2024-06-01 20:44:06 -0300

    refactor: screenshots

--- lib/utils/fiveflix_strings.dart
+++ lib/utils/fiveflix_strings.dart
@@ -36,5 +36,5 @@ abstract class FiveflixStrings {
   static const errorDefault = 'Server error. Please try again later.';
   static const storyLineIsEmpty = 'No story line available.';
 
-  static const apikey = '38100df4d45182409fff8c4b690f1be3';
+  static const apikey = 'INSERT_YOUR_API_KEY_HERE';
 }
