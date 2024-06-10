3004588653ed6a0e165d08fb43aa8fd54d88a696
jusylopes <jusylopes@outlook.com>
2024-06-10 13:22:24 -0300

    feat: favorite button test Co-authored-by: Evelyn <evelyn.vanielly@gmail.com>

--- test/screens/widgets/favorite_button_test.dart
+++ test/screens/widgets/favorite_button_test.dart
@@ -1,7 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fiveflix/blocs/bloc_exports.dart';
+import 'package:flutter_fiveflix/models/models_exports.dart';
+import 'package:flutter_fiveflix/screens/media_detail/widgets/media_detail_widgets_exports.dart';
+import 'package:flutter_fiveflix/utils/utils_exports.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'favorite_button_test.mocks.dart';
 
+@GenerateMocks([FavoriteBloc])
 void main() {
-  testWidgets('favorite button ...', (tester) async {
-    // TODO: Implement test
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Favorite Widget Tests |', () {
+    late MockFavoriteBloc favoriteBloc;
+    late MediaModel testMedia;
+
+    setUp(() {
+      favoriteBloc = MockFavoriteBloc();
+
+      testMedia = MediaModel(
+          id: 653346,
+          title: "Kingdom of the Planet of the Apes",
+          genreIds: [
+            878,
+            12,
+            28,
+          ],
+          voteAverage: 8.0,
+          overview:
+              "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
+          releaseDate: DateTime(2024 - 05 - 08),
+          popularity: 5120.32,
+          posterPath: "/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
+          voteCount: 843,
+          backdropPath: "/fqv8v6AycXKsivp1T5yKtLbGXce.jpg");
+
+      when(favoriteBloc.state)
+          .thenReturn(FavoriteGetAllSuccessState(items: const []));
+
+      when(favoriteBloc.stream)
+          .thenAnswer((_) => Stream<FavoriteState>.fromIterable([
+                FavoriteGetAllSuccessState(items: const []),
+                FavoriteItemAddedState(item: testMedia),
+              ]));
+    });
+
+    Future<void> createWidget(WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiBlocProvider(
+          providers: [
+            BlocProvider<FavoriteBloc>.value(
+              value: favoriteBloc,
+            ),
+          ],
+          child: MaterialApp(
+            home: Scaffold(
+              body: FavoriteButton(itemFavorite: testMedia),
+            ),
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+    }
+
+    testWidgets(
+        'Verify FavoriteButton shows check icon and SnackBar on adding to favorites',
+        (WidgetTester tester) async {
+      await createWidget(tester);
+
+      expect(find.byIcon(Icons.add), findsOneWidget);
+      await tester.tap(find.byType(FloatingActionButton));
+
+      when(favoriteBloc.state)
+          .thenReturn(FavoriteItemAddedState(item: testMedia));
+
+      await tester.pumpAndSettle();
+
+      expect(find.text(FiveflixStrings.addItemFavoriteSucess), findsOneWidget);
+
+      // expect(find.byIcon(Icons.check), findsOneWidget);
+    });
   });
 }
