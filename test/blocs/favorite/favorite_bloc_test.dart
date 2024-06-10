35988cc06f90af4d92b60a63706e277bbe8d3b99
WillianSeidel <willianrobertoseidel@gmail.com>
2024-06-09 22:40:00 -0300

    feat: test favorite
    Co-authored-by: NocFidback <90123470+Lucasnoc@users.noreply.github.com>

--- test/blocs/favorite/favorite_bloc_test.dart
+++ test/blocs/favorite/favorite_bloc_test.dart
@@ -1,7 +1,95 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_fiveflix/blocs/bloc_exports.dart';
+import 'package:flutter_fiveflix/models/models_exports.dart';
+import 'package:flutter_fiveflix/repositories/favorite_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'favorite_bloc_test.mocks.dart';
 
+@GenerateMocks([FavoriteRepository])
 void main() {
-  test('favorite bloc ...', () {
-    // TODO: Implement test
+  group('Favorite Bloc |', () {
+    late MockFavoriteRepository mockFavoriteRepository;
+    late MediaModel favoriteItem;
+    late List favoriteItems;
+
+    setUp(() {
+      mockFavoriteRepository = MockFavoriteRepository();
+      favoriteItem = MediaModel(
+        id: 1,
+        title: 'Test Movie',
+        genreIds: [1, 2],
+        voteAverage: 8.0,
+        overview: 'Test Overview',
+        releaseDate: DateTime(1999, 9, 2),
+        popularity: 2,
+        posterPath: '',
+        voteCount: 2,
+        backdropPath: '',
+      );
+      favoriteItems = [favoriteItem];
+    });
+
+///// FavoriteToggleEvent()
+    ///
+///// FavoriteGetAllEvent()
+    blocTest<FavoriteBloc, FavoriteState>(
+      'emits [FavoriteLoadingState, FavoriteCategoriesSucessState] when [FavoriteGetAllEvent] is added success.',
+      setUp: () {
+        when(mockFavoriteRepository.getAllMedias())
+            .thenAnswer((_) async => favoriteItems);
+      },
+      build: () => FavoriteBloc(repository: mockFavoriteRepository),
+      act: (bloc) => bloc.add(const FavoriteGetAllEvent()),
+      expect: () => <FavoriteState>[
+        FavoriteLoadingState(),
+        FavoriteGetAllSuccessState(items: favoriteItems),
+      ],
+    );
+
+    blocTest<FavoriteBloc, FavoriteState>(
+      'emits [FavoriteLoadingState, FavoriteCategoriesSucessState] when [FavoriteGetAllEvent] is added and fetch fails.',
+      build: () {
+        when(mockFavoriteRepository.getAllMedias()).thenThrow(Exception());
+        return FavoriteBloc(repository: mockFavoriteRepository);
+      },
+      act: (bloc) => bloc.add(const FavoriteGetAllEvent()),
+      expect: () => <FavoriteState>[
+        FavoriteLoadingState(),
+        FavoriteErrorState(Exception().toString()),
+      ],
+    );
+
+    ///// FavoriteRemoveEVent()
+    blocTest<FavoriteBloc, FavoriteState>(
+      'emits [FavoriteLoadingState, FavoriteCategoriesSucessState] when [FavoriteRemoveItemEvent] is added success.',
+      setUp: () {
+        when(mockFavoriteRepository.deleteMedia(favoriteItem.id))
+            .thenAnswer((_) async => favoriteItem.id);
+      },
+      build: () => FavoriteBloc(repository: mockFavoriteRepository),
+      act: (bloc) => bloc.add(
+        FavoriteRemoveItemEvent(id: favoriteItem.id),
+      ),
+      expect: () => <FavoriteState>[
+        FavoriteItemRemovedState(),
+      ],
+    );
+
+    blocTest<FavoriteBloc, FavoriteState>(
+      'emits [FavoriteLoadingState, FavoriteCategoriesSucessState] when [FavoriteRemoveItemEvent] is added success.',
+      build: () {
+        when(mockFavoriteRepository.deleteMedia(favoriteItem.id))
+            .thenThrow(Exception());
+        return FavoriteBloc(repository: mockFavoriteRepository);
+      },
+      act: (bloc) => bloc.add(
+        FavoriteRemoveItemEvent(id: favoriteItem.id),
+      ),
+      expect: () => <FavoriteState>[
+        FavoriteErrorState(Exception().toString()),
+      ],
+    );
   });
 }
