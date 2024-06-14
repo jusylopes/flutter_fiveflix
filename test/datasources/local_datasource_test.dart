d81e0e2dc800812a12d3fd088bf6537b54bc1218
jusylopes <jusylopes@outlook.com>
2024-06-14 02:45:21 -0300

    refactor: review and revise tests

---
+++ test/datasources/local_datasource_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('local datasource ...', (tester) async {
+    // TODO: Implement test
+  });
+}
---
+++ test/screens/news_screen_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('news screen ...', (tester) async {
+    // TODO: Implement test
+  });
+}
