5ce890b098d1bcab6bb7d55b5bf6253e72de678e
jusylopes <jusylopes@outlook.com>
2024-08-19 20:34:46 -0300

    refactor: update PopularMediaCards to include "See all" button

--- lib/screens/popular_screen/widgets/popular_media_cards.dart
+++ lib/screens/popular_screen/widgets/popular_media_cards.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fiveflix/models/models_exports.dart';
+import 'package:flutter_fiveflix/screens/all_medias/all_media_screen.dart';
+import 'package:flutter_fiveflix/screens/all_medias/all_media_type.dart';
 import 'package:flutter_fiveflix/screens/widgets/widgets_exports.dart';
 
 class PopularMediaCards extends StatelessWidget {
   final List<MediaModel> medias;
   final String titleMedia;
+  final AllMediaType mediaType;
+  final String idGenre;
 
-  const PopularMediaCards({
-    super.key,
-    required this.medias,
-    required this.titleMedia,
-  });
+  const PopularMediaCards(
+      {super.key,
+      required this.medias,
+      required this.titleMedia,
+      required this.mediaType,
+      this.idGenre = ''});
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +23,35 @@ class PopularMediaCards extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5.0),
-          child: Text(
-            titleMedia,
-            style: Theme.of(context).textTheme.titleMedium,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                titleMedia,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AllMediaScreen(
+                                mediaType: mediaType,
+                                titleMedia: titleMedia,
+                                idGenre: idGenre,
+                              )));
+                },
+                child: Text(
+                  'See all',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
           ),
+        ),
+        const SizedBox(
+          height: 6,
         ),
         Column(
           children: [
