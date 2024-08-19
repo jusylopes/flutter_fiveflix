109c402226b0b705ae7e04e0443d50c8df922ee1
jusylopes <jusylopes@outlook.com>
2024-08-19 20:30:51 -0300

    feat: add AllMediaBloc for fetching all media

--- lib/blocs/blocs_exports.dart
+++ lib/blocs/blocs_exports.dart
@@ -6,6 +6,7 @@ export 'package:flutter_fiveflix/blocs/search/search_bloc.dart';
 export 'package:flutter_fiveflix/blocs/favorite/favorite_bloc.dart';
 export 'package:flutter_fiveflix/blocs/categories/categories_bloc.dart';
 export 'package:flutter_fiveflix/blocs/games/game_bloc.dart';
+export 'package:flutter_fiveflix/blocs/all_media/all_media_bloc.dart';
 
 export 'package:flutter_fiveflix/blocs/bloc_providers.dart';
 export 'package:equatable/equatable.dart';
